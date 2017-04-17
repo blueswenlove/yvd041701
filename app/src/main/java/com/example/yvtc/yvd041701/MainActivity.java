cc280fece56e531dfd24724845b87f34aeb944a0
blueswen <blueswenlove@gamil.com>
2017-04-17 11:16:46 +0800

    Initial commit

---
+++ app/src/main/java/com/example/yvtc/yvd041701/MainActivity.java
@@ -0,0 +1,92 @@
+package com.example.yvtc.yvd041701;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+public class MainActivity extends AppCompatActivity {
+    InputStream inputStream;
+    MyDataHandler dataHandler;
+    TextView tv;
+    ListView lv;
+    ArrayAdapter<String> adapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // tv = (TextView) findViewById(R.id.textView);
+        lv = (ListView) findViewById(R.id.listView);
+        dataHandler = new MyDataHandler();
+        new Thread(){
+            @Override
+            public void run() {
+                super.run();
+                try {
+                    URL url = new URL("https://udn.com/rssfeed/news/1");
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.setRequestMethod("GET");
+                    conn.connect();
+                    inputStream = conn.getInputStream();
+
+                    BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+                    final StringBuilder sb = new StringBuilder();
+                    String str;
+                    while ((str = br.readLine()) != null)
+                    {
+                        sb.append(str);
+                    }
+                    Log.d("MYNETLog", sb.toString());
+
+                    SAXParserFactory spf = SAXParserFactory.newInstance();
+                    SAXParser sp = spf.newSAXParser();
+                    XMLReader xr = sp.getXMLReader();
+                    xr.setContentHandler(dataHandler);
+                    xr.parse(new InputSource(new StringReader(sb.toString())));
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            // tv.setText(sb.toString());
+                            adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, dataHandler.titles);
+                            lv.setAdapter(adapter);
+                        }
+                    });
+
+
+
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+
+                } catch (ParserConfigurationException e) {
+                    e.printStackTrace();
+                } catch (SAXException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }.start();
+
+    }
+}
---
+++ app/src/main/java/com/example/yvtc/yvd041701/MyDataHandler.java
@@ -0,0 +1,58 @@
+package com.example.yvtc.yvd041701;
+
+import android.util.Log;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+import java.util.ArrayList;
+
+/**
+ * Created by yvtc on 2017/4/17.
+ */
+
+public class MyDataHandler extends DefaultHandler {
+    boolean isTitle = false;
+    boolean inItem = false;
+    
+    public ArrayList<String> titles = new ArrayList<>();
+    public ArrayList<String>  links = new ArrayList<>();
+    @Override
+    public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+        super.startElement(uri, localName, qName, attributes);
+        if (qName.equals("title"))
+        {
+            isTitle = true;
+        }
+        if (qName.equals("item"))
+        {
+            inItem = true;
+        }
+    }
+
+    @Override
+    public void endElement(String uri, String localName, String qName) throws SAXException {
+        super.endElement(uri, localName, qName);
+        if (qName.equals("title"))
+        {
+            isTitle = false;
+        }
+        if (qName.equals("item"))
+        {
+            inItem = false;
+        }
+
+    }
+
+    @Override
+    public void characters(char[] ch, int start, int length) throws SAXException {
+        super.characters(ch, start, length);
+        if (isTitle && inItem)
+        {
+            String str = new String(ch).substring(start, length);
+            titles.add(str);
+            Log.d("TITLE", str);
+        }
+    }
+}
